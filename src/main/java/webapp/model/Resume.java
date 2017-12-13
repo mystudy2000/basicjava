addf435ca3bfe5f167970b7111f6a7069bdb3555
mystudy2000 <32429829+mystudy2000@users.noreply.github.com>
2017-12-13 15:00:02 +0300

    Lesson 03 equals&hashCode

--- src/main/java/webapp/model/Resume.java
+++ src/main/java/webapp/model/Resume.java
@@ -1,11 +1,23 @@
 package webapp.model;
+import java.util.UUID;
 /*** Resume class */
 public class Resume {
 
     // Unique identifier
 public  String uuid;
-public String getUuid() {return uuid; }
-public void   setUuid(String uuid) {this.uuid = uuid;}
+public  String getUuid() {return uuid; }
+public  void   setUuid(String uuid) {this.uuid = uuid;}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
     @Override
     public String toString() {
         return uuid;
--- src/main/java/webapp/storage/ArrayStorage.java
+++ src/main/java/webapp/storage/ArrayStorage.java
@@ -1,11 +1,16 @@
 package webapp.storage;
 import webapp.model.Resume;
+import java.util.Arrays;
 
 /*** Array based storage for Resumes */
 public class ArrayStorage {
+    private static final int STORAGE_LIMIT = 10000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
+    private int size;
 
-    private final Resume[] storage = new Resume[10000];
-    private int size = 0;
+    public ArrayStorage() {
+        size = 0;
+    }
 
     public void clear() {
         for (int i = 0; i < size; i++) storage[i] = null;
@@ -25,7 +30,7 @@ public class ArrayStorage {
     public void save(Resume r) {
         if (getIndex(r.getUuid()) != -1) {
                         System.out.println("Resume " + r.getUuid() + " already exist");
-                    } else if (size >= storage.length) {
+                    } else if (size >= STORAGE_LIMIT) {
                         System.out.println("Storage overflow");
                     } else {
                         storage[size] = r;
