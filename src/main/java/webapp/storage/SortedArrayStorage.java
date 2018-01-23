289b2debccab475e6f7da809df8627e144af27d3
mystudy2000 <32429829+mystudy2000@users.noreply.github.com>
2018-01-23 20:25:55 +0300

    Lesson06 Lambda

--- src/main/java/webapp/storage/SortedArrayStorage.java
+++ src/main/java/webapp/storage/SortedArrayStorage.java
@@ -9,13 +9,7 @@ import java.util.UUID;
 public class SortedArrayStorage extends AbstractArrayStorage{
 
 
-    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
-    @Override
-        public int compare(Resume o1, Resume o2) {
-        return o1.getUuid().compareTo(o2.getUuid());
-        }
-    };
-
+    private static final Comparator<Resume> RESUME_COMPARATOR = (o1, o2) -> o1.getUuid().compareTo(o2.getUuid());
 
     @Override
     protected void doSave(Resume r) {
