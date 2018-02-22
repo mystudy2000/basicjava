ea1b3452cca9ed4ab3a2e96df782fa97d2b7d6c8
mystudy2000 <32429829+mystudy2000@users.noreply.github.com>
2018-02-22 19:25:36 +0300

    Lesson08 DateTime api

---
+++ src/main/java/webapp/util/DateUtil.java
@@ -0,0 +1,9 @@
+package webapp.util;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class DateUtil {
+    public static LocalDate of(int year, Month month) {
+        return LocalDate.of(year, month, 1);
+    }
+}
