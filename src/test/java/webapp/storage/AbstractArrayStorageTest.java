fdb03c328e7d986bffe3a73020c73c6c2796be37
mystudy2000 <32429829+mystudy2000@users.noreply.github.com>
2018-04-27 09:11:47 +0300

    exceptionStorageOverflowTest correction

--- src/test/java/webapp/storage/AbstractArrayStorageTest.java
+++ src/test/java/webapp/storage/AbstractArrayStorageTest.java
@@ -1,5 +1,6 @@
 package webapp.storage;
 
+import org.junit.Assert;
 import org.junit.Test;
 import webapp.exceptions.StorageException;
 import webapp.model.Resume;
@@ -15,8 +16,13 @@ public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
     public void exceptionStorageOverflowTest() throws StorageException {
         // --Exception: storage overflow test ---*/
         storage.clear();
-        for (int i = 0; i < STORAGE_LIMIT; i++) {
-            storage.save(new Resume("One more name " + i));
+        try {
+            for (int i = 0; i < STORAGE_LIMIT; i++) {
+                storage.save(new Resume("One more name " + i));
+            }
+        }
+        catch (StorageException e) {
+            Assert.fail();
         }
         storage.save(new Resume("This is overflow!!!"));
     }
