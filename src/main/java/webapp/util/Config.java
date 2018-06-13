a409cf6bb868666a122d1d38a791542e487aac89
mystudy2000 <32429829+mystudy2000@users.noreply.github.com>
2018-06-13 19:41:34 +0300

    HW 15 Servlet

--- src/main/java/webapp/util/Config.java
+++ src/main/java/webapp/util/Config.java
@@ -11,7 +11,8 @@ import java.util.Properties;
 
 
 public class Config {
-    private static final File PROPS = new File("./config/resumes.properties");
+  //  private static final File PROPS = new File("./config/resumes.properties");
+  private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
@@ -35,5 +36,14 @@ public class Config {
 
     public File getStorageDir() {
         return storageDir;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
